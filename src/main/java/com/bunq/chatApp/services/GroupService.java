42a62e469feb35005f8e38ad43c8179fe40a2c2e
Suleyman Yilmaz <yilmazsgit@gmail.com>
2023-05-20 01:35:44 +0200

    method for getUserOfGroup added

--- src/main/java/com/bunq/chatApp/services/GroupService.java
+++ src/main/java/com/bunq/chatApp/services/GroupService.java
@@ -1,14 +1,21 @@
 package com.bunq.chatApp.services;
 
 import com.bunq.chatApp.entities.Group;
+import com.bunq.chatApp.entities.User;
+import com.bunq.chatApp.exception.NotFoundException;
 
 import java.util.List;
 
 public interface GroupService {
-    Group createGroup(String name);
+
+    Group createGroup(String name, Long creatorId) throws NotFoundException;
+
     Group getGroupByName(String name);
     List<Group> getAllGroups();
-    void addUserToGroup(String username, String groupName);
-    void removeUserFromGroup(String username, String groupName);
+
+    User addUserToGroup(String username, String groupName) throws NotFoundException;
+
+    void removeUserFromGroup(String username, String groupName) throws NotFoundException;
+    List<User> getUsersOfGroup(Long groupId) throws NotFoundException;
 }
 
