91c96843cc2c60008478955ffa2a5e5b2d17973f
Suleyman Yilmaz <yilmazsgit@gmail.com>
2023-05-20 01:39:09 +0200

    method and exception handling added

--- src/main/java/com/bunq/chatApp/services/UserService.java
+++ src/main/java/com/bunq/chatApp/services/UserService.java
@@ -1,8 +1,10 @@
 package com.bunq.chatApp.services;
 
 import com.bunq.chatApp.entities.User;
+import com.bunq.chatApp.exception.NotFoundException;
 
 public interface UserService {
     User createUser(String username, String firstName, String lastName);
     User getUserByUsername(String username);
+    User getUserByUserId(Long userId) throws NotFoundException;
 }
--- src/main/java/com/bunq/chatApp/services/UserServiceImpl.java
+++ src/main/java/com/bunq/chatApp/services/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.bunq.chatApp.services;
 
 import com.bunq.chatApp.entities.User;
+import com.bunq.chatApp.exception.NotFoundException;
 import com.bunq.chatApp.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,9 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User createUser(String username, String firstName, String lastName) {
+        User isExist = userRepository.findByUsername(username);
+        if(isExist != null)
+            throw new IllegalArgumentException("Username is already exist!");
         User user = new User(username, firstName, lastName);
         return userRepository.save(user);
     }
@@ -20,5 +24,10 @@ public class UserServiceImpl implements UserService {
     @Override
     public User getUserByUsername(String username) {
         return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public User getUserByUserId(Long userId) throws NotFoundException {
+        return userRepository.findById(userId).orElseThrow(() -> new NotFoundException("User is not found!"));
     }
 }
